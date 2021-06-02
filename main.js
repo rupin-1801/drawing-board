b02bdf56e772511e7489d39e3c707fd6c224d3fa
rupin-1801 <rupinbhugra1411@gmail.com>
2021-06-02 13:21:51 +0530

    added brush function to draw anything on board

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const canv = document.querySelector('canvas');
 const ctx = canv.getContext('2d');
 
+let strokeWidth = 1, pressed = false;
+
 window.onload = window.onresize = function(){
     let width = canv.width = window.innerWidth;
     let height = canv.height = window.innerHeight;
@@ -8,3 +10,22 @@ window.onload = window.onresize = function(){
     ctx.fillRect(0, 0, width, height);
 }
 
+document.onmousemove = draw;
+document.onmousedown = () => pressed = true;;
+document.onmouseup = () => pressed = false;;
+
+function degToRad(angle){
+    return angle * Math.PI / 180;
+}
+
+function draw(e){
+    if(pressed){
+        let posX = e.clientX;
+        let posY = e.clientY;
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.arc(posX, posY, strokeWidth, degToRad(0), degToRad(360), false);
+        ctx.fill();
+        requestAnimationFrame(draw);
+    }
+}
