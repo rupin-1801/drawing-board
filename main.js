56ede4ae4f0eb1d1c337cc24bc00ef1434397488
rupin-1801 <rupinbhugra1411@gmail.com>
2021-06-02 13:13:00 +0530

    painted canvas for onload and onresize events

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const canv = document.querySelector('canvas');
-let width = canv.width = window.innerWidth;
-let height = canv.height = window.innerHeight;
 const ctx = canv.getContext('2d');
+
+window.onload = window.onresize = function(){
+    let width = canv.width = window.innerWidth;
+    let height = canv.height = window.innerHeight;
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillRect(0, 0, width, height);
+}
+
